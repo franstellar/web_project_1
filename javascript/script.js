5474e2af5b5758a010f7532f078dbc5bbdc8baf7
fran <franferrarifloriano@gmail.com>
2026-03-03 00:37:30 -0300

    fix video

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,7 @@
 function carregarVideo(div) {
     div.innerHTML = `
         <iframe width="720" height="405" 
-        src="https://www.youtube.com/embed/_b6opMEu-zM?autoplay=1" 
+        src="https://www.youtube.com/embed/HV-m-0cUM6g?autoplay=1" 
         title="Just Another Day - Oingo Boingo" frameborder="0" 
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
         allowfullscreen></iframe>
