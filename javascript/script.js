02bc434c0ada7bab929a05934c7955b59c203ead
fran <franferrarifloriano@gmail.com>
2026-03-03 00:09:10 -0300

    update

---
+++ javascript/script.js
@@ -0,0 +1,9 @@
+function carregarVideo(div) {
+    div.innerHTML = `
+        <iframe width="560" height="315" 
+        src="https://www.youtube.com/embed/HV-m-0cUM6g?autoplay=1" 
+        title="YouTube video player" frameborder="0" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+        allowfullscreen></iframe>
+    `;
+}
